5538d5daed8a293cbe8f2d8c73fdc536369a17c6
Flambé <emilycogsdill@gmail.com>
2025-02-21 10:18:49 -0700

    delete old files

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_csv, name='upload_csv'),
-    path('filter/', views.filter_csv, name='filter_csv'),
-]
