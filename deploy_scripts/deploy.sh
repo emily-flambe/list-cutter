887eba5a2f7f0298c5273a7d5c09fba74b29bfff
Flambé <emilycogsdill@gmail.com>
2025-02-21 15:43:05 -0700

    Update deploy.sh

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # deploy-dev.sh
 
+# error noisily
+set -e
+
 if [ "$1" == "true" ]; then
   echo "Changes detected in Dockerfile or docker-compose.yml. Rebuilding Docker container..."
   docker-compose build && docker-compose up -d
@@ -15,5 +18,11 @@ docker-compose exec web python manage.py migrate
 echo "Collecting static files..."
 docker-compose exec web python manage.py collectstatic --noinput
 
-echo "Running frontend setup and build..."
-docker-compose exec web bash -l -c "cd frontend && npm install && npm run build && nohup npm run dev > /dev/null 2>&1 &"
+echo "Installing frontend dependencies..."
+docker-compose exec web bash -l -c "cd frontend && npm install"
+
+echo "Building frontend..."
+docker-compose exec web bash -l -c "cd frontend && npm run build"
+
+echo "Starting frontend development server..."
+docker-compose exec web bash -l -c "cd frontend && nohup npm run dev > /dev/null 2>&1 &"
