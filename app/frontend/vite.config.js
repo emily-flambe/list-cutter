7e60f84829758403f50ba2cca594be9e2d1fd7ec
Flambé <emilycogsdill@gmail.com>
2025-02-21 12:45:19 -0700

    Update vite.config.js

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,13 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-    ? 'https://list-cutter.emilyflam.be/'
-    : '/',
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8000',
-    },
-  },
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
