341cc1e2e2af1b26db002d18450f76ec1cc785e4
Flambé <emilycogsdill@gmail.com>
2025-02-22 22:54:43 -0700

    fix App.jsx (it was borked idk)

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -4,6 +4,30 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import FileUpload from './components/FileUpload';
 import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+// Function to get CSS variable values
+const getCssVariable = (variable) => {
+  return getComputedStyle(document.documentElement).getPropertyValue(variable).trim();
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: getCssVariable('--secondary'), // Get the CSS variable value
+    },
+    secondary: {
+      main: getCssVariable('--accent'), // Get the CSS variable value
+    },
+    background: {
+      default: getCssVariable('--primary-bg'), // Get the CSS variable value
+    },
+    text: {
+      primary: getCssVariable('--primary-text'), // Get the CSS variable value
+      secondary: getCssVariable('--secondary'), // Get the CSS variable value
+    },
+  },
+});
 
 function App() {
   return (
