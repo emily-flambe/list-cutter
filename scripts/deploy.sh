b4530e46496b49126b12582ccea482f0d8869d99
Flambé <emilycogsdill@gmail.com>
2025-02-23 22:51:45 -0700

    docker strats work IM GOING TO BED

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,16 +10,7 @@ docker system prune -a -f --volumes
 echo "Pulling latest images from Docker Hub..."
 docker-compose -f docker-compose.web.yml pull
 
-if [ "$1" == "true" ]; then
-  echo "Changes detected in Dockerfile or docker-compose.yml. Rebuilding Docker container..."
-  
-  # Bring down any existing containers and remove orphans
-  docker-compose down --remove-orphans
-  
-  # Build and bring up containers with orphan removal
-  docker-compose -f docker-compose.web.yml build && docker-compose -f docker-compose.web.yml up -d --remove-orphans
-else
-  echo "No rebuild required. Restarting container..."
-  docker-compose -f docker-compose.web.yml restart
-fi
+echo "Bringing up containers..."
+docker-compose -f docker-compose.web.yml up -d --remove-orphans
 
+echo "Deployment complete!"
