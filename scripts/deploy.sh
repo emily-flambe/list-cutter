653b3326c3ba4315fd614e3549bf53784bc3ff06
Flambé <emilycogsdill@gmail.com>
2025-02-23 17:15:32 -0700

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,18 +19,3 @@ else
   echo "No rebuild required. Restarting container..."
   docker-compose restart
 fi
-
-echo "Running Django migrations..."
-docker-compose exec backend python manage.py migrate
-
-echo "Collecting static files..."
-docker-compose exec backend python manage.py collectstatic --noinput
-
-echo "Installing frontend dependencies..."
-docker-compose exec frontend bash -l -c "npm install"
-
-echo "Building frontend..."
-docker-compose exec frontend bash -l -c "npm run build"
-
-echo "Starting frontend development server..."
-docker-compose exec frontend bash -l -c "nohup npm run dev > /dev/null 2>&1 &"
