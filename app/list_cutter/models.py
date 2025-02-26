1018904fde1fe9630245c78323ebb282ebdd97d1
Flambé <emilycogsdill@gmail.com>
2025-02-25 20:40:08 -0700

    add data model UserFile

--- app/list_cutter/models.py
+++ app/list_cutter/models.py
@@ -1,1 +1,12 @@
 # app/list_cutter/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.file.name} uploaded by {self.user.username}"
