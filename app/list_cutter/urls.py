9cad6d47018b767b97b9584a387d48a0640bba5c
Flambé <emilycogsdill@gmail.com>
2025-02-26 16:26:57 -0700

    Update urls.py

--- app/list_cutter/urls.py
+++ app/list_cutter/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("csv_cutter/", upload_file_for_csv_cutter, name="csv_cutter"),
     path("export_csv/", export_csv, name="export_csv"),
     path("upload/", upload_file, name="upload_file"),
-    path("files/", list_uploaded_files, name="list_uploaded_files"),
+    path("list_uploaded_files/", list_uploaded_files, name="list_uploaded_files"),
     path("download/<str:filename>/", download_file, name="download_file"),
 ]
 
