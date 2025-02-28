1c18509e70e33b6d9b1eb2877006e773f9c5ee60
Flambé <emilycogsdill@gmail.com>
2025-02-27 19:31:00 -0700

    Update generate_data.py

--- app/list_cutter/common/generate_data.py
+++ app/list_cutter/common/generate_data.py
@@ -1,8 +1,9 @@
 from list_cutter.models import SavedFile
 from list_cutter.graph_models import SavedFileNode
 import json
+import random
 
-def create_saved_file_node(saved_file):
+def create_saved_file_node(saved_file, cut_to_node=None):
     """Creates a SavedFileNode from a SavedFile instance."""
     metadata = json.dumps(saved_file.metadata) if saved_file.metadata else '{}'
     saved_file_node = SavedFileNode(
@@ -12,4 +13,9 @@ def create_saved_file_node(saved_file):
         metadata=metadata
     )
     saved_file_node.save()
+    
+    # Connect CUT_TO relationship if provided
+    if cut_to_node:
+        saved_file_node.CUT_TO.connect(cut_to_node)
+    
     return saved_file_node 
