433298140b75ddfa16b9efee19502cb82bfd123d
Flambé <emilycogsdill@gmail.com>
2025-02-27 19:30:59 -0700

    Update create_savedfiles.py

--- app/list_cutter/management/commands/create_savedfiles.py
+++ app/list_cutter/management/commands/create_savedfiles.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from list_cutter.models import SavedFile
 from list_cutter.common.generate_data import create_saved_file_node
 import random
+import uuid
 
 '''
 Run this to create saved files and nodes with relationships. This is useful for testing.
@@ -22,10 +23,12 @@ class Command(BaseCommand):
         # Create 5 files in a linear relationship
         for i in range(5):
             # Create a SavedFile instance
+            file_id = uuid.uuid4()
             saved_file = SavedFile.objects.create(
                 user=user,
                 file_name=f"file_{random.getrandbits(16):x}.csv",
                 file_path=f"uploads/file_{random.getrandbits(16):x}.csv",
+                file_id=file_id,  # Include the generated file_id
                 metadata={"foo": "keke"}
             )
             saved_files.append(saved_file)
@@ -36,7 +39,7 @@ class Command(BaseCommand):
 
             # Create relationships between the nodes
             if i > 0:
-                saved_file_nodes[i].created_from.connect(saved_file_nodes[i - 1])
+                saved_file_nodes[i].CUT_FROM.connect(saved_file_nodes[i - 1])
 
         # Create two additional files derived from the first file (A)
         for j in range(2):
@@ -52,7 +55,7 @@ class Command(BaseCommand):
             saved_file_nodes.append(additional_file_node)
 
             # Connect additional files to the first file (A)
-            additional_file_node.created_from.connect(saved_file_nodes[0])
+            additional_file_node.CUT_FROM.connect(saved_file_nodes[0])
 
         # Create relationships for one of the additional files to derive two more files
         for k in range(2):
@@ -68,7 +71,7 @@ class Command(BaseCommand):
             saved_file_nodes.append(derived_file_node)
 
             # Connect derived files to one of the additional files
-            saved_file_nodes[5].created_from.connect(derived_file_node)
+            saved_file_nodes[5].CUT_FROM.connect(derived_file_node)
 
         # Create one more file derived from one of the derived files
         final_file = SavedFile.objects.create(
@@ -83,6 +86,6 @@ class Command(BaseCommand):
         saved_file_nodes.append(final_file_node)
 
         # Connect the final file to one of the derived files
-        saved_file_nodes[6].created_from.connect(final_file_node)
+        saved_file_nodes[6].CUT_FROM.connect(final_file_node)
 
         self.stdout.write(self.style.SUCCESS("Successfully created SavedFile and SavedFileNode records with specified relationships.")) 
